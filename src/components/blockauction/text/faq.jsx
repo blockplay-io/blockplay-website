67b416decc79b17bfdee3d83310357fac995f5cc
Shefass <shefass@gmail.com>
2019-09-03 14:17:42 +0300

    initial

---
+++ src/components/blockauction/text/faq.jsx
@@ -0,0 +1,134 @@
+
+
+export const  faqText = [
+    {
+      id: 1,
+      textEng: {
+        title: "To which address should I send BURST to bid in a auction?",
+        text:
+          `Always send BURST to the smart contract address which is called "Auction address", never to the current auction leader.`
+      },
+      textCh: {
+        title: "Sorry, no translation",
+       
+      }
+    },
+    {
+      id: 2,
+      textEng: {
+        title: "How is the winner established?",
+        text:
+         "After time out, the smart contract will finish and sends to auction owner biggest bid. All transactions are public in the Burst blockchain, "+
+         "so seller and buyer can see that it is fair."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 3,
+      textEng: {
+        title: "What is time out?",
+        text:
+          "Time out is a time after which the smart contract will finish. After this time the auctions smart contract will be not active and it will not refund transactions,  so don't send to it."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 4,
+      textEng: {
+        title: "If I win a auction, what should I do?",
+        text:
+          "Congratulations, take a screenshot and share with your friends! Now you just have to wait for auction owner to contact you and ask for "+
+          "the real world address, so he can send your items to you."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 5,
+      textEng: {
+        title: "How auction owner will contact me?",
+        text:
+          "Auction owner will send you message to Burst address which you used in the auction."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 6,
+      textEng: {
+        title: "Who is auction owner?",
+        text:
+          "Auction owner is a person who is beneficiary of the auction. He/she is responsable of a items delivery or quality."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 7,
+      textEng: {
+        title: "How I can create own auction?",
+        text:
+          "You can use public auctions smart contract code by compiling it with BlockTalk and putting it in the Burst blockchain or just contact us, we will help you."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 8,
+      textEng: {
+        title: "Can I bid if there is other bid in line?",
+        text:
+          "You can bid in auction anytime. If there are multiple bids in line, all smaller bids will be refunded."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 9,
+      textEng: {
+        title: "I changed my mind. Give my Burst back!",
+        text:
+          "All transactions in the Burst blockchain are irreversible. Burst is a decentralized network, so you are the owner of your funds and have all "+
+          "freedoms to use it how you want, but with big freedom comes big responsibility."
+      },
+      textCh: {
+        title: "Sorry, no translation",
+      }
+    },
+    {
+      id: 10,
+      textEng: {
+        title: "Where the 30 BURST go (activation fee)?",
+        text:
+          "Every smart contract running in the blockchain cost BURST to" +
+          " run, collected by miners. The block auction smart contract costs around 30 BURST to run."
+      },
+      textCh: {
+        title: "30 BURST的用意是什么（激活费）？",
+        text: "在区块链中运行的每个智能合约都需要BURST来运营，并由矿工收集。该区块冠军的智能合约需约30 BURST的成本。"
+      }
+    },
+    {
+      id: 11,
+      textEng: {
+        title: "What is the minimum transaction fee?",
+        text:
+          " Every transaction in Burst blockchain cost a fee to be processed. Higher fees have higher priority, you should have" +
+          "no problem if you choose the standard fee."
+      },
+      textCh: {
+        title: "什么是最低交易费？",
+        text: "Burst区块链中的每笔交易都需要付费才能获得处理。更高的费用具有更高的优先级，但如果您选择标准费用，您也应该不会有问题。"
+      }
+    }
+  ];
+  

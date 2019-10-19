c1aba933b52280107fd321a76e3e05bb9d2912c3
Shefass <shefass@gmail.com>
2019-10-19 19:43:04 +0300

    back

--- src/constants/const.js
+++ src/constants/const.js
@@ -55,10 +55,10 @@ export const pastAuctionWinners = [
 export const EXPLORERFUN = "https://testnet.explorer.burstcoin.network/?action=account&account=";
 export const EXPLORER = "https://explorer.burstcoin.network/?action=account&account=";
 
-export const NODE = "http://127.0.0.1:8125";
+
 //export const NODE = "http://testnet.getburst.net:6876";
 export const NODEFUN = "https://test-burst.megash.it:443";
-//export const NODE = "https://wallet.burstcoin.ro:443";
+export const NODE = "https://wallet.burstcoin.ro:443";
 //const NODE = 'https://burst-now-proxy.ohager.now.sh';
 
 

f504826276b9eb7e9e6db902201e55dc280b5dbe
unknown <otekdo.corporation@gmail.com>
2021-05-31 20:51:03 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   env: {
     REACT_APP_PROD_DOMAIN: "https://blockplay.io/",
   },
   future: {
     webpack5: true,
   },
-};
+});
